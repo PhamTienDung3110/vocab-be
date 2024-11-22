ab8fffba70e01baab2bea68cc46167d14e6d0870
Pham Tien Dung (HN-BSD-Dev <dungpt1@runsystem.local>
2024-11-22 17:42:16 +0700

    udpate porrt

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5001;
+const PORT = 0;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
