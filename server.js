2ec989cb3f3d1dcd7a7344423a2aa35622a6de8b
dungpt3 <phamtiendung0926@gmail.com>
2024-11-25 02:30:11 +0700

    update be

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const PORT = 0;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
